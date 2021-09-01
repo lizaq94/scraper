cb776628999185fd562e91410644ae3d850c4d0d
lizaq94 <domagala.k94@gmail.com>
2021-09-01 23:08:14 +0200

    download offer image and price from page

--- index.js
+++ index.js
@@ -6,28 +6,47 @@ const axios = require('axios');
 
 const url = `https://www.olx.pl/nieruchomosci/dzialki/sprzedaz/sejny/?search%5Bdist%5D=10`;
 
-async function creatOffer(data) {
-  const dom = new JSDOM(data);
-
-  const document = dom.window.document;
-  const offer = Array.from(document.querySelectorAll('div.offer-wrapper')).map(
-    (compact) => ({
-      title: compact.querySelector('a.link strong').textContent,
-      url: compact.querySelector('a.link').getAttribute('href'),
-    })
-  );
-
-  console.log(offer);
-}
-
-async function getOffer() {
+async function getAllOferts(url) {
   try {
     const response = await axios.get(url);
     const data = response.data;
-    await creatOffer(data);
+    return data;
   } catch (error) {
-    console.log('Erorr is ', error);
+    console.log('Error:', error);
   }
 }
 
-getOffer();
+async function getDataFromOffert(data) {
+  const dom = new JSDOM(data);
+  const { document } = dom.window;
+  const offerData = [];
+
+  const offers = Array.from(document.querySelectorAll('div.offer-wrapper')).map(
+    (offer) => offer
+  );
+
+  offers.forEach((offer) => {
+    const title = offer.querySelector('a.link strong').textContent;
+    const image = offer.querySelector('img.fleft')
+      ? offer.querySelector('img.fleft').src
+      : 'brak zdjęcia';
+    const url = offer.querySelector('h3 a.link').getAttribute('href');
+    const price = Number(
+      offer
+        .querySelector('p.price strong')
+        .textContent.slice(0, -2)
+        .split(' ')
+        .join('')
+    );
+    offerData.push({
+      title,
+      image,
+      url,
+      price,
+    });
+  });
+
+  console.log(offerData);
+}
+
+getAllOferts(url).then((response) => getDataFromOffert(response));
